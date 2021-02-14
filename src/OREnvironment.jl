8d949a4958fd3d6fb03c197c557790cd79788a12
DavidGarHeredia <dgheredia@protonmail.com>
2021-02-14 14:22:10 +0100

    Files generated by PkgTemplates

---
+++ src/OREnvironment.jl
@@ -0,0 +1,5 @@
+module OREnvironment
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using OREnvironment
+using Test
+
+@testset "OREnvironment.jl" begin
+    # Write your tests here.
+end
