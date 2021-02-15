ef0cbdadadea1b2e4e82f2a31caa024ca9c83d88
DavidGarHeredia <dgheredia@protonmail.com>
2021-02-15 22:18:49 +0100

    status moved to a different file

--- src/Solution.jl
+++ src/Solution.jl
@@ -1,20 +1,6 @@
 ############################
 # Types & Constructors
 ############################
-Base.@kwdef mutable struct DefaultStatus{T<:Real, G<:Real} <: Status
-    feasible::Bool = false;
-    optimal::Bool  = false; 
-    objfunction::T = zero(T);
-    constraintLhsConsumption::Array{G,1};
-end
-
-function constructStatus(numberConstraints::Int, 
-                         typeObjFunction::DataType, 
-                         typeLHS::DataType)
-    lhs = zeros(typeLHS, numberConstraints);
-    return DefaultStatus{typeObjFunction, typeLHS}(constraintLhsConsumption = lhs);
-end
-
 mutable struct FixLengthArray{T<:Real} <: Solution
     sol::Array{T,1};
     status::Status;
@@ -31,39 +17,6 @@ function constructSolution(::Val{:FixLengthArray},
     return FixLengthArray(zeros(typeVariables, numVariables), status);
 end
 
-############################
-# General methods for Status
-############################
-is_feasible(s::Status)::Bool           = s.feasible;
-set_feasible!(s::Status, val::Bool)    = s.feasible = val;
-is_optimal(s::Status)::Bool            = s.optimal; 
-set_optimal!(s::Status, val::Bool)     = s.optimal = val; 
-get_objfunction(s::Status)             = s.objfunction;
-set_objfunction!(s::Status, val::Real) = s.objfunction = val;
-get_constraint_consumption(s::Status, pos::Int) = @inbounds s.constraintLhsConsumption[pos];
-set_constraint_consumption!(s::Status, val::Real, pos::Int) = @inbounds s.constraintLhsConsumption[pos] = val;
-
-worst_value(objsense::Symbol, T::DataType) = worst_value(Val(objsense), T);
-worst_value(::Val{:max}, T::DataType) = typemin(T);
-worst_value(::Val{:min}, T::DataType) = typemax(T);
-
-function is_first_status_better(s1::Status, 
-                                s2::Status, 
-                                objSense::Symbol, 
-                                feasibilityRequiered::Bool)::Bool
-    if feasibilityRequiered
-        if is_feasible(s1) && !is_feasible(s2)
-            return true;
-        elseif !is_feasible(s1) && is_feasible(s2)
-            return false;
-        end
-    end
-    # In any other situation we compare the objective function 
-    return is_first_obj_function_better(s1, s2, Val(objSense));
-end
-
-is_first_obj_function_better(s1::Status, s2::Status, ::Val{:max})::Bool = get_objfunction(s1) > get_objfunction(s2);
-is_first_obj_function_better(s1::Status, s2::Status, ::Val{:min})::Bool = get_objfunction(s1) < get_objfunction(s2);
 
 ############################
 # General methods for Solution
@@ -117,17 +70,6 @@ function update_constraint_consumption_and_feasibility!(s::Solution,
     return nothing;
 end
 
-############################
-# Specific methods for Status
-############################
-function update_solution_status!(s::Solution, 
-                                 st::DefaultStatus{T,G}) where {T<:Real, G<:Real}
-    set_objfunction!(s, get_objfunction(st));
-    set_feasible!(s, is_feasible(st));
-    set_optimal!(s, is_optimal(st)); 
-    copy!(s.status.constraintLhsConsumption, st.constraintLhsConsumption);
-    return nothing;
-end
 
 ############################
 # Specific methods for Solution
@@ -170,8 +112,9 @@ function remove_all_solutions!(s::FixLengthArray{T}) where {T<:Real}
     return nothing;
 end
 
-@inline function copy_first_solution_to_second!(s1::FixLengthArray{T}, s2::FixLengthArray{T}) where {T<:Real}
-    update_solution_status!(s2, s1.status); 
+@inline function copy_first_solution_to_second!(s1::FixLengthArray{T}, 
+                                                s2::FixLengthArray{T}) where {T<:Real}
+    update_status!(s2.status, s1.status); 
     copy!(s2.sol, s1.sol);
     return nothing;
 end
