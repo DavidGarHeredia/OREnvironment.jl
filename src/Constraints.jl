228b50e527d55e73219247498d4da776a0061336
DavidGarHeredia <dgheredia@protonmail.com>
2021-02-16 12:23:25 +0100

    moving methods to Solution.jl

--- src/Constraints.jl
+++ src/Constraints.jl
@@ -6,8 +6,10 @@ mutable struct DefaultConstraint{T<:Real} <: Constraint
     variablesNegativeCoefficients::Dict{Int,T}; 
 end
 
-function constructConstraint(rhs::T, constraintType::Symbol, 
-                             variables::Array{Int,1}, coefficients::Array{T,1}) where {T<:Real}
+function constructConstraint(rhs::T, 
+                             constraintType::Symbol, 
+                             variables::Array{Int,1}, 
+                             coefficients::Array{T,1}) where {T<:Real}
     dictPositive = Dict{Int,T}();
     dictNegative = Dict{Int,T}();
     for i in 1:length(variables)
@@ -47,7 +49,8 @@ function get_coefficient(c::Constraint, pos::Int)
     return val;
 end
 
-function get_relationship_variables_constraints(constraints::Array{<:Constraint,1}, nVariables::Int)
+function get_relationship_variables_constraints(constraints::Array{<:Constraint,1}, 
+                                                nVariables::Int)
     if length(constraints) == 0 return Array{Array{Int,1}, 1}() end
     variablesConstraints = [Int[] for i in 1:nVariables];
     for i in 1:length(constraints)
@@ -59,56 +62,6 @@ function get_relationship_variables_constraints(constraints::Array{<:Constraint,
         end
     end
     return variablesConstraints;
-end
-
-function compute_lhs(c::Constraint, s::Solution)
-   local lhs = zero(typeof(get_rhs(c)));
-   for (variable, coef) in c.variablesPositiveCoefficients
-       lhs += coef*get_solution(s, variable);
-   end
-   for (variable, coef) in c.variablesNegativeCoefficients
-       lhs += coef*get_solution(s, variable);
-   end
-   return lhs;
-end
-
-function is_feasible(s::Solution, constraints::Array{<:Constraint,1})::Bool
-    local N::Int = length(constraints);
-    if N == 0 return true end
-    for c in constraints
-        local lhs = compute_lhs(c, s);
-        if !is_feasible(c, lhs)
-            return false;
-        end
-    end
-    return true;
-end 
-
-function is_increment_feasible(s::Solution, constraints::Array{<:Constraint,1}, variable::Int, 
-                               Δvariable::Real, idxConstraints::Array{Int,1})::Bool
-    local N::Int = length(idxConstraints);
-    if N == 0 return true end;
-    local isIncrementFeasible::Bool = true;
-    @inbounds for i in idxConstraints
-        isIncrementFeasible = is_increment_feasible(s, constraints[i], i, variable, Δvariable);
-        if !isIncrementFeasible
-            return false;
-        end
-    end
-    return true;
-end
-
-function compute_lhs_increment(s::Solution, c::Constraint, posConstraint::Int,
-                               variable::Int, Δvariable::Real)
-    local Δconsumption = Δvariable*get_coefficient(c, variable);
-    local lhs = get_constraint_consumption(s, posConstraint) + Δconsumption; 
-    return lhs;
-end
-
-function is_increment_feasible(s::Solution, c::Constraint, posConstraint::Int,
-                               variable::Int, Δvariable::Real)::Bool
-    lhs = compute_lhs_increment(s, c, posConstraint, variable, Δvariable);
-    return is_feasible(c, lhs);
 end
 
 is_feasible(c::Constraint, lhs::Real)::Bool = is_feasible(get_rhs(c), lhs, Val(get_type(c))); 
