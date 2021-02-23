5431c3bbdb0d5de4d0a818bfa7e239ab68339de8
DavidGarHeredia <dgheredia@protonmail.com>
2021-02-23 19:19:54 +0100

    methods for Porblem.jl

---
+++ src/Problem.jl
@@ -0,0 +1,37 @@
+
+mutable struct DefaultProblem{T<:Real} <: Problem
+  costs::Array{T,1};
+  constraints::Array{<:Constraint,1};
+  variablesConstraints::Array{Array{Int,1},1};
+  objSense::Symbol;
+end
+
+function constructProblem(costs::Array{T,1},
+                          constraints::Array{<:Constraint,1},
+                          objSense::Symbol) where {T<:Real}
+  variablesConstraints = get_relationship_variables_constraints(constraints, length(costs));
+  return DefaultProblem(costs, constraints, variablesConstraints, objSense);
+end
+
+get_cost(p::Problem, pos::Int)             = p.costs[pos];
+set_cost!(p::Problem, val::Real, pos::Int) = p.costs[pos] = val;
+get_obj_sense(p::Problem)::Symbol          = p.objSense;
+set_obj_sense!(p::Problem, val::Symbol)    = p.objSense = val;
+get_constraint(p::Problem, pos::Int)       = p.constraints[pos];
+get_constraints_of_variable(p::Problem, pos::Int)::Array{Int,1} = p.variablesConstraints[pos];
+
+function add_constraint!(p::Problem, c::Constraint) 
+  push!(p.constraints, c);
+  local idx::Int = length(p.constraints);
+  add_constraint_index_to_variables!(c, idx, p.variablesConstraints);
+end
+
+function add_constraint!(p::Problem, constraints::Array{<:Constraint,1}) 
+  for c in constraints
+    add_constraint!(p,c);
+  end
+end
+
+gap(lowerBound::Real, upperBound::Real, objSense::Symbol) = return gap(lowerBound, upperBound, Val(objSense));
+gap(lowerBound::Real, upperBound::Real, ::Val{:min}) = return (upperBound-lowerBound)/lowerBound;
+gap(lowerBound::Real, upperBound::Real, ::Val{:max}) = return (upperBound-lowerBound)/upperBound;
---
+++ test/Problem.jl
@@ -0,0 +1,202 @@
+using OREnvironment
+using Test
+
+@testset "building problem" begin 
+  cost = collect(1.0:6.0);
+  variables1 = [1, 3, 4, 6];
+  variables2 = [1, 3, 5, 6];
+  coefs1 = [2.3, 3.2, 3.1, 12.34];
+  coefs2 = coefs1 .+ 1.0;
+  constraint1 = OREnvironment.constructConstraint(15.0, :lessOrEq, variables1, coefs1);
+  constraint2 = OREnvironment.constructConstraint(9.0, :lessOrEq, variables2, coefs2);
+  constraints = [constraint1, constraint2];
+  variablesConstraints = [[1,2], Int[], [1,2], [1], [2], [1,2]];
+  p = OREnvironment.DefaultProblem(cost, constraints, variablesConstraints, :max);
+
+  for i in 1:length(cost)
+    @test OREnvironment.get_cost(p, i) == cost[i];
+  end
+  for i in 1:length(variablesConstraints)
+    @test OREnvironment.get_constraints_of_variable(p, i) == variablesConstraints[i];
+  end
+  for i in 1:length(cost)
+    OREnvironment.set_cost!(p, 12.0*i,  i); 
+  end
+  for i in 1:length(cost)
+    @test OREnvironment.get_cost(p, i) == 12.0*i;
+  end
+  @test OREnvironment.get_obj_sense(p) == :max;
+  OREnvironment.set_obj_sense!(p, :min);
+  @test OREnvironment.get_obj_sense(p) == :min;
+
+  @test OREnvironment.get_constraint(p, 1) === constraint1;
+  @test OREnvironment.get_constraint(p, 1) == constraint1;
+  @test OREnvironment.get_constraint(p, 2) === constraint2;
+  @test OREnvironment.get_constraint(p, 2) == constraint2;
+
+  @test OREnvironment.is_variable(OREnvironment.get_constraint(p,1), 1) == true;
+  @test OREnvironment.is_variable(OREnvironment.get_constraint(p,1), 2) == false;
+  @test OREnvironment.is_variable(OREnvironment.get_constraint(p,2), 4) == false;
+  @test OREnvironment.is_variable(OREnvironment.get_constraint(p,2), 5) == true;
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,1)) == 15.0;
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,2)) == 9.0;
+  @test OREnvironment.get_type(OREnvironment.get_constraint(p,2)) == :lessOrEq;
+  @test OREnvironment.get_coefficient(OREnvironment.get_constraint(p,2), 5) == 4.1;
+  @test OREnvironment.get_coefficient(OREnvironment.get_constraint(p,2), 4) == 0.0;
+end
+
+@testset "building problem as in practice" begin
+  # read constraints one by one in a for loop and save them
+  # obtain the variables constraints with the method
+  # build the problem
+  cost = rand(6);
+  data_coefs = [rand(3), rand(2), rand(4)];
+  data_vars  = [[1, 2, 3], [3, 6], [3, 4, 5, 6]];
+  data_rhs = [12.3, 9.0, 33.0];
+  data_symbol = [:lessOrEq, :equal, :greaterOrEq];
+  # simulating the reading of constraints
+  constraints = Array{OREnvironment.Constraint, 1}();
+  for i in 1:3
+    vars = data_vars[i]; # in practice we read from file
+    coefs = data_coefs[i];
+    rhs = data_rhs[i];
+    symbol = data_symbol[i];
+    push!(constraints, OREnvironment.constructConstraint(rhs, symbol, vars, coefs));
+  end
+  variablesConstraints = OREnvironment.get_relationship_variables_constraints(constraints, 6);
+  p = OREnvironment.constructProblem(cost, constraints, :max);
+
+  # test that everything works
+  @test length(constraints) == 3;
+  @test length(p.constraints) == 3;
+  for i in 1:3
+    @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,i)) == data_rhs[i];
+    @test OREnvironment.get_type(OREnvironment.get_constraint(p,i)) == data_symbol[i];
+    @test OREnvironment.get_constraint(p, i) == constraints[i];
+    @test OREnvironment.get_constraint(p, i) === constraints[i];
+  end
+  for i in 1:6
+    @test OREnvironment.get_cost(p,i) == cost[i];
+    @test OREnvironment.get_constraints_of_variable(p, i) == variablesConstraints[i];
+  end
+
+  # simulating adding more constraints one by one
+  constraint4 = OREnvironment.constructConstraint(3.9, :equal, [5, 6], [1.4, 11.0]);
+  OREnvironment.add_constraint!(p, constraint4);
+  @test length(p.constraints) == 4;
+  @test length(constraints) == 4; # this is because p has a pointer to constraints!!!
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,4)) == 3.9;
+  @test OREnvironment.get_type(OREnvironment.get_constraint(p,4)) == :equal;
+  @test OREnvironment.get_constraint(p, 4) == constraint4;
+  @test OREnvironment.get_constraint(p, 4) === constraint4;
+  variablesConstraints = OREnvironment.get_relationship_variables_constraints(constraints, 6);
+  for i in 1:6
+    @test OREnvironment.get_constraints_of_variable(p, i) == variablesConstraints[i];
+  end
+
+  constraint5 = OREnvironment.constructConstraint(5.5, :greaterOrEq, [3, 6], [2.4, 11.0]);
+  OREnvironment.add_constraint!(p, constraint5);
+  @test length(p.constraints) == 5;
+  @test length(constraints) == 5; # this is because p has a pointer to constraints!!!
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,5)) == 5.5;
+  @test OREnvironment.get_type(OREnvironment.get_constraint(p,5)) == :greaterOrEq;
+  @test OREnvironment.get_constraint(p, 5) == constraint5;
+  @test OREnvironment.get_constraint(p, 5) === constraint5;
+  variablesConstraints = OREnvironment.get_relationship_variables_constraints(constraints, 6);
+  for i in 1:6
+    @test OREnvironment.get_constraints_of_variable(p, i) == variablesConstraints[i];
+  end
+  
+  # simulating adding more constraints as a vector
+  constraint6 = OREnvironment.constructConstraint(6.9, :equal, [5, 6], [1.4, 11.0]);
+  constraint7 = OREnvironment.constructConstraint(7.9, :lessOrEq, [5, 6], [1.4, 11.0]);
+  newConstraints = [constraint6, constraint7];
+  OREnvironment.add_constraint!(p, newConstraints);
+  @test length(p.constraints) == 7;
+  @test length(constraints) == 7; # this is because p has a pointer to constraints!!!
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,6)) == 6.9;
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,7)) == 7.9;
+  @test OREnvironment.get_type(OREnvironment.get_constraint(p,6)) == :equal;
+  @test OREnvironment.get_type(OREnvironment.get_constraint(p,7)) == :lessOrEq;
+  @test OREnvironment.get_constraint(p, 6) == constraint6;
+  @test OREnvironment.get_constraint(p, 6) === constraint6;
+  @test OREnvironment.get_constraint(p, 7) == constraint7;
+  @test OREnvironment.get_constraint(p, 7) === constraint7;
+  variablesConstraints = OREnvironment.get_relationship_variables_constraints(constraints, 6);
+  for i in 1:6
+    @test OREnvironment.get_constraints_of_variable(p, i) == variablesConstraints[i];
+  end
+end
+
+function build_problem()
+  cost = collect(1.0:6.0);
+  data_coefs = [[1.1, 1.2, 1.3], [2.1, 2.2], [3.1, 3.2, 3.3, 3.4]];
+  data_vars  = [[1, 2, 3], [3, 6], [3, 4, 5, 6]];
+  data_rhs = [12.3, 9.0, 33.0];
+  data_symbol = [:lessOrEq, :equal, :greaterOrEq];
+  # simulating the reading of constraints
+  constraints = Array{OREnvironment.Constraint, 1}();
+  for i in 1:3
+    vars = data_vars[i]; # in practice we read from file
+    coefs = data_coefs[i];
+    rhs = data_rhs[i];
+    symbol = data_symbol[i];
+    push!(constraints, OREnvironment.constructConstraint(rhs, symbol, vars, coefs));
+  end
+  variablesConstraints = OREnvironment.get_relationship_variables_constraints(constraints, 6);
+  p = OREnvironment.constructProblem(cost, constraints, :max);
+  return p;
+end
+
+@testset "scope of problem" begin
+  # test that there is no issue with the scope of problem!
+  p = build_problem();
+  data_rhs = [12.3, 9.0, 33.0];
+  data_symbol = [:lessOrEq, :equal, :greaterOrEq];
+  @test length(p.constraints) == 3;
+  for i in 1:3
+    @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,i)) == data_rhs[i];
+    @test OREnvironment.get_type(OREnvironment.get_constraint(p,i)) == data_symbol[i];
+  end
+  @test OREnvironment.get_coefficient(p.constraints[1], 1) == 1.1;
+  @test OREnvironment.get_coefficient(p.constraints[1], 2) == 1.2;
+  @test OREnvironment.get_coefficient(p.constraints[1], 3) == 1.3;
+  @test OREnvironment.get_coefficient(p.constraints[1], 4) == 0.0;
+  @test OREnvironment.get_coefficient(p.constraints[2], 3) == 2.1;
+  @test OREnvironment.get_coefficient(p.constraints[2], 6) == 2.2;
+  @test OREnvironment.get_coefficient(p.constraints[2], 5) == 0.0;
+  @test OREnvironment.get_coefficient(p.constraints[3], 3) == 3.1;
+  @test OREnvironment.get_coefficient(p.constraints[3], 4) == 3.2;
+  @test OREnvironment.get_coefficient(p.constraints[3], 5) == 3.3;
+  @test OREnvironment.get_coefficient(p.constraints[3], 6) == 3.4;
+  for i in 1:6
+    @test OREnvironment.get_cost(p,i) == i*1.0;
+  end
+  @test OREnvironment.get_constraints_of_variable(p, 1) == [1]; 
+  @test OREnvironment.get_constraints_of_variable(p, 2) == [1]; 
+  @test OREnvironment.get_constraints_of_variable(p, 3) == [1,2,3]; 
+  @test OREnvironment.get_constraints_of_variable(p, 4) == [3]; 
+  @test OREnvironment.get_constraints_of_variable(p, 5) == [3]; 
+  @test OREnvironment.get_constraints_of_variable(p, 6) == [2,3]; 
+
+  constraint4 = OREnvironment.constructConstraint(3.9, :equal, [5, 6], [1.4, 11.0]);
+  OREnvironment.add_constraint!(p, constraint4);
+  @test length(p.constraints) == 4;
+  @test OREnvironment.get_rhs(OREnvironment.get_constraint(p,4)) == 3.9;
+  @test OREnvironment.get_type(OREnvironment.get_constraint(p,4)) == :equal;
+  @test OREnvironment.get_constraint(p, 4) == constraint4;
+  @test OREnvironment.get_constraint(p, 4) === constraint4;
+  @test OREnvironment.get_constraints_of_variable(p, 1) == [1]; 
+  @test OREnvironment.get_constraints_of_variable(p, 2) == [1]; 
+  @test OREnvironment.get_constraints_of_variable(p, 3) == [1,2,3]; 
+  @test OREnvironment.get_constraints_of_variable(p, 4) == [3]; 
+  @test OREnvironment.get_constraints_of_variable(p, 5) == [3, 4]; 
+  @test OREnvironment.get_constraints_of_variable(p, 6) == [2,3,4]; 
+end
+
+@testset "gap function" begin 
+  LB = 7.0;
+  UB = 9.0;
+  @test OREnvironment.gap(LB, UB, :min) == (UB-LB)/LB;
+  @test OREnvironment.gap(LB, UB, :max) == (UB-LB)/UB;
+end
