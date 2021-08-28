30f542fb8415a310f3ff7620f7a4513b196f1ad3
DavidGarHeredia <dgheredia@protonmail.com>
2021-08-29 00:21:18 +0200

    movements to implement

---
+++ src/Movements.jl
@@ -0,0 +1,7 @@
+# flip value
+# symmetric value... like flip but from lb to ub
+# replace.. it is a simple add and update
+# swap two values
+# inverse (inverse the order of all elements between x_i and x_j) 
+# insert (put element x_j in position i) # check if Julia is faster by default 
+# insert_sequence = move a whole seq instead of just one element like insertion
