7b309b4e1ba7047ffebddc01c73e93b59cc43454
DavidGarHeredia <dgheredia@protonmail.com>
2021-10-19 22:03:41 +0200

    trying to fix documentation ci

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 push!(LOAD_PATH,"../")
 
-using Documenter, OREnvironment, ORInterface
+using Documenter, OREnvironment
 
 makedocs(sitename="OREnvironment.jl", 
 format = Documenter.HTML(prettyurls = false)
